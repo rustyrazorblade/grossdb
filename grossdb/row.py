a6c5babe083d66a484f365152f72667edb44e62c
Jon Haddad <jon@jonhaddad.com>
2016-02-15 15:21:52 -0800

    empty files

---
+++ grossdb/row.py
@@ -0,0 +1,3 @@
+
+class Row(object):
+    _data = {}
---
+++ grossdb/table.py
@@ -0,0 +1,9 @@
+class Table(object):
+    _rows = None
+    _indexes = None
+
+    def __init__(self):
+        self._rows = {}
+        self._indexes = {}
+
+
