fcbbdbe05d0e86d9cc992ebb6eecfe123e73bb7b
Jon Haddad <jon@jonhaddad.com>
2016-02-15 17:59:47 -0800

    keeping track of total records

--- grossdb/table.py
+++ grossdb/table.py
@@ -5,6 +5,7 @@ class Table(object):
     _rows = None
     _indexes = None
     _name = None
+    _total = 0
 
     def __init__(self, name):
         self._rows = {}
@@ -18,7 +19,9 @@ class Table(object):
 
     def insert(self, row):
         # TODO schema validation
-        pass
+        record_id = self._total + 1
+        self._rows[record_id] = row
+        self._total += 1
 
 
 
